7fa2c51c8ebc0771c4a5702748d82f535845cff5
zhonggaorong <458673541@qq.com>
2016-03-15 13:50:26 +0800

    Initial Commit

---
+++ OptimizationControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OptimizationControllerDemo
+//
+//  Created by 张国荣 on 16/3/15.
+//  Copyright © 2016年 111. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
